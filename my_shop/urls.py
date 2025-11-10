c67bd3bc22ac0a0adc19d7b909a69a18238fd883
Esp <gipno262@gmail.com>
2025-11-10 16:05:43 +0300

    hw_lesson_22_3

--- my_shop/urls.py
+++ my_shop/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from catalog import views
+from django.urls import path, include  # ← Обязательно добавь include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
-    path('contacts/', views.contacts, name='contacts'),
+    path('', include('catalog.urls')),  # ← Подключаем маршруты из приложения catalog
 ]
