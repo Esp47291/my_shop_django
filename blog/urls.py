387dca90d4003072ada20dc9c43c94c099ce2893
Esp <gipno262@gmail.com>
2025-11-20 16:01:44 +0300

    Complete FBV / CBV hm 2

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
-from . import views
+from .views_without_auth import (  # Импортируем из нового файла
+    BlogPostListView, BlogPostDetailView,
+    BlogPostCreateView, BlogPostUpdateView, BlogPostDeleteView
+)
 
-app_name = 'blog'  # Добавляем namespace
+app_name = 'blog'
 
 urlpatterns = [
-    path('', views.BlogPostListView.as_view(), name='blogpost_list'),
-    path('<int:pk>/', views.BlogPostDetailView.as_view(), name='blogpost_detail'),
-    path('create/', views.BlogPostCreateView.as_view(), name='blogpost_create'),
-    path('<int:pk>/edit/', views.BlogPostUpdateView.as_view(), name='blogpost_edit'),
-    path('<int:pk>/delete/', views.BlogPostDeleteView.as_view(), name='blogpost_delete'),
+    path('', BlogPostListView.as_view(), name='blogpost_list'),
+    path('<int:pk>/', BlogPostDetailView.as_view(), name='blogpost_detail'),
+    path('create/', BlogPostCreateView.as_view(), name='blogpost_create'),
+    path('<int:pk>/edit/', BlogPostUpdateView.as_view(), name='blogpost_edit'),
+    path('<int:pk>/delete/', BlogPostDeleteView.as_view(), name='blogpost_delete'),
 ]
